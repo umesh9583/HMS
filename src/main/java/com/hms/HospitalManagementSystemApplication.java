5776139691cf9e2a5493b684f8973ed847412d9a
UMESH CHANDRA MOHANTA <mohantaumeshchandra2002@gmail.com>
2025-05-14 18:46:16 +0530

    na

--- src/main/java/com/hms/HospitalManagementSystemApplication.java
+++ src/main/java/com/hms/HospitalManagementSystemApplication.java
@@ -7,5 +7,6 @@ package com.hms;
    public class HospitalManagementSystemApplication {
        public static void main(String[] args) {
            SpringApplication.run(HospitalManagementSystemApplication.class, args);
+           System.out.println("Running");
        }
    }
